c5c5e7bf60a3c22249cbc7b2d43fa581f2d61e37
Julie Cook <juliecook328@gmail.com>
2019-06-30 21:13:50 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,12 @@
+  #method name      #parameter
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+#introduction_with_language
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction_with_language_optional(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
